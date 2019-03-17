c248f0aee0eadeb6a2150d51b59508771b7a01bf
Kristin <kristinstone@hotmail.com>
2019-03-17 16:37:48 +1030

    active navigation

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -41,3 +41,16 @@ function imageGallery() {
 imageGallery();
 
 
+//active nav
+
+function activeMenu() {
+
+    var url = window.location.href;
+
+    $('.nav-inner a').filter(function() {
+        return this. href == url;
+
+    }).addClass('active')
+}
+
+activeMenu();
