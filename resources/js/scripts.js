a5425b0040f8821c49cd270eb1a3caa04cc3d4e7
Kristin <kristinstone@hotmail.com>
2019-03-17 15:35:51 +1030

    image gallery complete

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -3,15 +3,27 @@ cssVars();
 
 //open and close nav
 
-$('.nav-prompt').click(function()  {
+$('.nav-prompt').click(function () {
 
-if ($('.nav-inner').attr('aria-expanded') == 'false') {
+    if ($('.nav-inner').attr('aria-expanded') == 'false') {
 
-    $('.nav-inner').attr('aria-expanded', 'true');
+        $('.nav-inner').attr('aria-expanded', 'true');
 
-} else {
-    $('.nav-inner').attr('aria-expanded', 'false');
+    } else {
+        $('.nav-inner').attr('aria-expanded', 'false');
+
+    }
+
+});
+
+// image gallery
+
+function imageGallery() {
+
+    if ($('.image-gallery').length) {
+        $ ('.image-gallery a') .simpleLightbox();
+    }
 
 }
 
-});
+imageGallery();
